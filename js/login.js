a259bad0b00f56489f8e5862e870710b91b7309a
reactjsuser02 <dummyemail@mail.com>
2024-01-08 21:44:36 +0500

    login script

--- js/login.js
+++ js/login.js
@@ -7,10 +7,6 @@ const list = document.querySelector("#list");
 const btn = document.querySelector("button");
 const form = document.querySelector("form");
 
-console.log('first')
-console.log(localStorage.getItem('login'))
-console.log(localStorage.getItem('password'))
-
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   let login = e.target[0].value;
@@ -19,13 +15,8 @@ form.addEventListener("submit", (e) => {
   e.target[1].value = "";
   localStorage.setItem('login', login)
   localStorage.setItem('password', password)
-  console.log('second')
-  console.log(localStorage.getItem('login'))
-  console.log(localStorage.getItem('password'))
   window.location.href = "index.html";
 });
-
-// localStorage.clear()
 
 
 
